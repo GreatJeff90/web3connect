12439269965592ceb0451ed51375ba7459d8cc47
Vendor Ventory <vendorventory@gmail.com>
2026-03-27 21:59:18 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,21 @@
-import { defineConfig,splitVendorChunkPlugin  } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
- export default defineConfig({
-  plugins: [react(),    obfuscatorPlugin({
-    include: ["src/path/to/file.js", "path/anyjs/**/*.js", /foo.js$/],
-    exclude: [/node_modules/],
-    apply: "build",
-    debugger: true,
-    options: {
-      debugProtection: true,
-    },
-  }),
+export default defineConfig({
+  plugins: [
+    react(), 
+    nodePolyfills(), 
+    obfuscatorPlugin({
+      include: ["src/path/to/file.js", "path/anyjs/**/*.js", /foo.js$/],
+      exclude: [/node_modules/],
+      apply: "build",
+      debugger: true,
+      options: {
+        debugProtection: true,
+      },
+    }),
   ],
   build: {
     chunkSizeWarningLimit: 1600,
@@ -22,4 +26,3 @@ import obfuscatorPlugin from "vite-plugin-javascript-obfuscator";
     },
   },
 })
-  
